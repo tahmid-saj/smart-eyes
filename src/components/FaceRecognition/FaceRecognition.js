2f3ae2721b40a56825c84a13d0c2b433e7215ae8
Tahmid Sajin <tahmid.saj@gmail.com>
2023-04-23 05:42:54 -0400

    Updating facerecognition and nav. Adding signin, registration and nav routing.

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import './FaceRecognition.css';
 
-const FaceRecognition = ({ imageUrl }) => {
+const FaceRecognition = ({ box, imageUrl }) => {
     return (
         <div className="center ma">
             <div className="absolute mt2">
-                <img alt="face_image" 
+                <img id="inputimage" alt="face_image" 
                 src={imageUrl} width="500px" height="auto"></img>
+
+                <div className="bounding-box" style={{
+                    top: box.topRow, 
+                    right: box.rightCol, 
+                    bottom: box.bottomRow, 
+                    left: box.leftCol}}></div>
             </div>
         </div>
     );
