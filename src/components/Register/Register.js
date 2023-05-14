66e8855f7835fd78a297adac641a019a39632152
Tahmid Sajin <tahmid.saj@gmail.com>
2023-05-14 19:21:03 -0400

    Modifying register component to create user in psql

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -36,7 +36,7 @@ class Register extends React.Component {
             response.json();
         })
         .then((user) => {
-            if (user) {
+            if (user.id) {
                 this.props.loadUser(user);
                 this.props.onRouteChange("home");
             }
